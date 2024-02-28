f6028e1d2f5a2f2d7ff9f9576089eeb26ae1afd4
yenorii <144069387+Yenorii@users.noreply.github.com>
2024-02-28 13:00:23 -0500

    saving work

--- EmployeeSQL/employee.sql
+++ EmployeeSQL/employee.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE PewlettHackard;
 USE PewlettHackard;
 
 -- Create 'departments' table
@@ -111,3 +112,34 @@ SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM dept_manager dm
 JOIN departments d ON dm.dept_no = d.dept_no
 JOIN employees e ON dm.emp_no = e.emp_no;
+
+-- List department number for each employee along with their employee number, last name, first name, and department name.
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_no, d.dept_name
+FROM dept_emp de
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter 'B'.
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+-- List the frequency counts, in descending order, of all the employee last names.
+SELECT last_name, COUNT(last_name) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
